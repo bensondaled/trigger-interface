fb4d2fa30ec0f5a927b202fd3549045e633a875d
Ben <bendeverett@gmail.com>
2014-03-31 23:33:25 -0400

    better

--- track_mouse3.py
+++ track_mouse3.py
@@ -10,7 +10,11 @@ cv = cv2.cv
 import os
 import json
 
-SHOW = True
+show = raw_input('Show? (0/1) ')
+show = int(show)
+
+SHOW = show
+RESAMPLE = 5
 
 cv2.namedWindow('img')
 cv2.namedWindow('diff')
@@ -20,14 +24,18 @@ cv2.namedWindow('motionmask')
 thresh = 15
 duration = 1
 seg_thresh = 100
-translation_thresh = 120
+translation_thresh = 130
 cth1 = 0 #canny thresh
 cth2 = 0
 diff_thresh = 100
+kernel = 15
 
 centers = []
+track = None
 left = 0
 right = 0
+left_assumed = 0
+right_assumed = 0
 skipped = 0
 
 def ginput(n):
@@ -37,17 +45,19 @@ def ginput(n):
     #   pts[idx][0] = height-p[0]
     return pts
 
-def get_frame(mov, n=1):
+def get_frame(mov, n=1, skip=0, blur=True):
+    for s in range(skip):
+        mov.read()
     if n==-1:
         n = 99999999999999999.
-    kernel =17 
     def get():
         valid, frame = mov.read()
         if not valid:
             return (False, None)
         frame = frame.astype(np.float32)
         frame = cv2.cvtColor(frame, cv2.cv.CV_RGB2GRAY)
-        frame = cv2.GaussianBlur(frame, (kernel,kernel), 0)
+        if blur:
+            frame = cv2.GaussianBlur(frame, (kernel,kernel), 0)
         return valid,frame
 
     valid,frame = get()
@@ -91,12 +101,17 @@ elif mode==TEST:
     trialdir = condition+'_test'
 
 try:
-    baseline = np.load(baseline_dir+'/baseline.npy')
+    baseline = np.load(baseline_dir+'/%s_baseline.npy'%baseline_dir)
 except:
     blmov = cv2.VideoCapture(os.path.join(baseline_dir, baseline_dir+'-cam0.avi'))
     valid, baseline = get_frame(blmov, n=-1)
     blmov.release()
-    np.save(baseline_dir+'/baseline', baseline)
+    np.save(baseline_dir+'/%s_baseline'%baseline_dir, baseline)
+    
+    blmov = cv2.VideoCapture(os.path.join(baseline_dir, baseline_dir+'-cam0.avi'))
+    valid, baseline_orig = get_frame(blmov, n=-1, blur=False)
+    blmov.release()
+    np.save(baseline_dir+'/%s_baseline_orig'%baseline_dir, baseline_orig)
 
 os.chdir(trialdir)
 timefile = [i for i in os.listdir('.') if 'timestamps' in i][0]
@@ -107,9 +122,10 @@ mov = cv2.VideoCapture(vidfile)
 #for i in range(1500):
 #    mov.read() #for testing
 height, width = np.shape(baseline)
+track = np.zeros(np.shape(baseline))
 valid,first = get_frame(mov)
 try:
-    pts = np.load('pts.npz')
+    pts = np.load('%s_pts.npz'%trialdir)
     pts_l = pts['pts_l']
     pts_r = pts['pts_r']
     pts_mouse = pts['pts_mouse']
@@ -122,7 +138,7 @@ except:
     print "Select mouse."
     pts_mouse = ginput(4)
     pl.close()
-    np.savez('pts', pts_l=pts_l, pts_r=pts_r, pts_mouse=pts_mouse)
+    np.savez('%s_pts'%trialdir, pts_l=pts_l, pts_r=pts_r, pts_mouse=pts_mouse)
 path_l, path_r = [mpl_path.Path(pts) for pts in [pts_l,pts_r]]
 last_center = np.round(np.mean(pts_mouse, axis=0)).astype(int)
 allpoints = np.append(pts_l,pts_r,axis=0)
@@ -133,10 +149,10 @@ bottom_border = np.max([i[1] for i in allpoints])
 
 idx = 0
 widgets=[' Iterating through images...', Percentage(), Bar()]
-pbar = ProgressBar(widgets=widgets, maxval=len(time)).start()
+pbar = ProgressBar(widgets=widgets, maxval=len(time)/RESAMPLE).start()
 
 while True:
-    valid,frame = get_frame(mov)
+    valid,frame = get_frame(mov,skip=RESAMPLE)
     if not valid:
         break
     diff = cv2.absdiff(frame,baseline)
@@ -148,36 +164,52 @@ while True:
     if len(possible) == 0:
         center = last_center
         skipped += 1
+        if path_l.contains_point(center):
+            left_assumed+=1
+        if path_r.contains_point(center):
+            right_assumed+=1
     else:
         chosen = possible[np.argmax([cv2.contourArea(c) for c in possible])]   
         center = contour_center(chosen)
         centers.append(center)
-    
-    if SHOW:
-        #display
-        showimg = np.copy(frame).astype(np.uint8)
+        track[center[1],center[0]] += 1
         if path_l.contains_point(center):
             left+=1
-            cv2.circle(showimg, tuple(center), radius=10, thickness=2, color=(255,255,255))
         if path_r.contains_point(center):
             right+=1
-            cv2.circle(showimg, tuple(center), radius=10, thickness=3,color=(255,255,255))
+    
+    #display
+    if SHOW:
+        showimg = np.copy(frame).astype(np.uint8)
         cv2.circle(showimg, tuple(center), radius=10, thickness=5, color=(255,255,255))
         cv2.imshow('img',showimg)
         cv2.imshow('diff', diff)
         cv2.waitKey(1)
-        #/display
+    #/display
 
     idx += 1
     last_center = center
     pbar.update(idx)
 pbar.finish()
-#disp_img = last
-#cv2.circle(disp_img, tuple(c), radius=3, color=(20,20,20)) for c in centers]
-#cv2.imwrite(moviedir+'_summary.png',disp_img)
+
+print "Skipped %i frames."%skipped
+results = {}
+results['n_frames'] = idx
+results['n_skipped'] = skipped
+results['frames_left'] = left
+results['frames_right'] = right
+results['frames_left-asssumed'] = left_assumed
+results['frames_right-assumed'] = right_assumed
+results['resample'] = RESAMPLE
+results['centers'] = [list(c) for c in centers]
+results['params'] = [thresh, duration, seg_thresh, translation_thresh, cth1, cth2, diff_thresh, kernel]
+results['params_key'] = ['thresh','duration','seg_thresh','translation_thresh','cth1','cth2','diff_thresh','kernel']
+resfile = open('%s_tracking_results.json'%trialdir,'w')
+resfile.write("%s"%json.dumps(results))
+resfile.close()
+np.save('%s_tracking'%trialdir,track)
 
 os.chdir(startdir)
 mov.release()
-cv2.destroyWindow('a')
-
+cv2.destroyAllWindows()
 
