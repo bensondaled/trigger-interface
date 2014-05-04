66203e35100163a771a1bf5a031958ac07b45cbb
Ben <bendeverett@gmail.com>
2014-05-04 19:44:07 -0400

    edits

--- core/daq.py
+++ core/daq.py
@@ -5,7 +5,7 @@ except:
 import numpy as np
 
 class Trigger(object):
-    def __init__(self, msg=[], duration=1.0, name='noname'):
+    def __init__(self, msg=[], duration=None, name='noname'):
         self.duration = duration
         self._msg = None
         self.msg = msg
@@ -16,7 +16,10 @@ class Trigger(object):
         return self._msg
     @msg.setter
     def msg(self, msg):
-        self._msg = np.array(msg).astype(np.uint8)
+        if type(msg) == list or type(msg) == np.array:
+            self._msg = np.array(msg).astype(np.uint8)
+        else:
+            self._msg = np.array(msg).astype(np.float64)
 
     def metadata(self):
         md = {}
@@ -26,21 +29,36 @@ class Trigger(object):
         return md
 
 class DAQ(object):
-    def __init__(self, port="Dev1/Port1/Line0:3"):
-        self.port = port
+    ANALOG = 0
+    DIGITAL = 1
+    def __init__(self, mode, port_digital="Dev1/Port1/Line0:3", port_analog="Dev1/ao0"):
+        self.mode = mode
+        if self.mode == self.ANALOG:
+            self.port = port_analog
+            self.minn = 0.0
+            self.maxx = 5.0
+        elif self.mode == self.DIGITAL:
+            self.port = port_digital
         self.clear_trig = Trigger(msg=[0,0,0,0])
         try:
             self.task = pydaq.TaskHandle()
             pydaq.DAQmxCreateTask("", pydaq.byref(self.task))
-            pydaq.DAQmxCreateDOChan(self.task, self.port, "OutputOnly", pydaq.DAQmx_Val_ChanForAllLines)
+            if self.mode == self.DIGITAL:
+                pydaq.DAQmxCreateDOChan(self.task, self.port, "OutputOnly", pydaq.DAQmx_Val_ChanForAllLines)
+            elif self.mode == self.ANALOG:
+                pydaq.DAQmxCreateAOVoltageChan(self.task, self.port,"", self.minn,self.maxx,pydaq.DAQmx_Val_Volts,None)
+            
             pydaq.DAQmxStartTask(self.task)
         except:
             self.task = None
             print "DAQ task did not successfully initialize."
     def trigger(self, trig):
         if self.task:
-            pydaq.DAQmxWriteDigitalLines(self.task,1,1,10.0,pydaq.DAQmx_Val_GroupByChannel,trig.msg,None,None)
-            pydaq.DAQmxWriteDigitalLines(self.task,1,1,10.0,pydaq.DAQmx_Val_GroupByChannel,self.clear_trig.msg,None,None)
+            if self.mode == self.DIGITAL:
+                pydaq.DAQmxWriteDigitalLines(self.task,1,1,10.0,pydaq.DAQmx_Val_GroupByChannel,trig.msg,None,None)
+                pydaq.DAQmxWriteDigitalLines(self.task,1,1,10.0,pydaq.DAQmx_Val_GroupByChannel,self.clear_trig.msg,None,None)
+            elif self.mode == self.ANALOG:
+                pydaq.DAQmxWriteAnalogF64(self.task,1,1,10.0,pydaq.DAQmx_Val_GroupByChannel,trig.msg,None,None)
         else:
             print "DAQ task not functional. Attempted to write %s."%str(trig.msg)
     def release(self):
--- imaging_interface.py
+++ imaging_interface.py
@@ -19,7 +19,7 @@ from core.daq import DAQ, Trigger
 from core.cameras import Camera
 
 class Experiment(object):
-    def __init__(self, camera=None, daq=None, mask_names=('WHEEL','EYE'),  motion_mask='WHEEL', movement_query_frames=20, movement_std_thresh=1.5, trigger_cycle=None, inter_trial_min=10.0, n_trials=-1, resample=1):
+    def __init__(self, camera=None, daq=None, mask_names=('WHEEL','EYE'),  motion_mask='WHEEL', movement_query_frames=10, movement_std_thresh=1.5, trigger_cycle=None, inter_trial_min=10.0, n_trials=-1, resample=1):
         """
         Parameters:
                 cameras: [list of] Camera object[s]
@@ -41,9 +41,10 @@ class Experiment(object):
         if type(daq) == DAQ:
             self.daq = daq
         elif daq==None:
-            self.daq = DAQ()
+            self.daq = DAQ(mode=DAQ.DIGITAL)
         else:
             raise Exception('No valid DAQ supplied.')
+        self.analog_daq = DAQ(mode=DAQ.ANALOG)
 
         # Set static parameters
         self.trigger_cycle = trigger_cycle
@@ -51,29 +52,41 @@ class Experiment(object):
         self.resample = resample
 
         # Set variable parameters
-        self.param_names = ['movement_std_threshold', 'movement_query_frames', 'inter_trial_min']
+        self.param_names = ['movement_std_threshold', 'movement_query_frames', 'inter_trial_min', 'wheel_translation','wheel_stretch','eye_translation','eye_stretch']
         self.params = {}
         self.params['movement_std_threshold'] = movement_std_thresh
         self.params['movement_query_frames'] = movement_query_frames
         self.params['inter_trial_min'] = inter_trial_min
+        self.params['wheel_translation'] = 50
+        self.params['wheel_stretch'] = 2
+        self.params['eye_translation'] = 50
+        self.params['eye_stretch'] = 1
+        
 
         # Setup interface
+        pl.ion()
+        self.fig = pl.figure()
+        self.ax = self.fig.add_subplot(111)
+        self.ax.set_ylim([-1,100])
+        self.plotdata = {m:self.ax.plot(np.arange(self.params['movement_query_frames']),np.zeros(self.params['movement_query_frames']), c)[0] for m,c in zip(self.mask_names,['r-','b-'])}  
+        self.plotline, = self.ax.plot(np.arange(self.params['movement_query_frames']), [self.params['movement_std_threshold']+self.params['wheel_translation'] for _ in range(self.params['movement_query_frames'])], 'k--')
         self.window = 'Interface'
-        self.plot = 'Plot'
         self.status = 'Status'
         self.controls = 'Controls'
+        self.params_win = 'Parameters'
         cv2.namedWindow(self.window, cv.CV_WINDOW_NORMAL)
-        cv2.namedWindow(self.plot, cv.CV_WINDOW_NORMAL)
         cv2.namedWindow(self.status, cv.CV_WINDOW_NORMAL)
         cv2.namedWindow(self.controls, cv.CV_WINDOW_NORMAL)
+        cv2.namedWindow(self.params_win, cv.CV_WINDOW_NORMAL)
         cv2.moveWindow(self.window, 210, 0)
-        cv2.moveWindow(self.plot, 540,0)
         cv2.moveWindow(self.status, 960, 0)
         cv2.moveWindow(self.controls, 0, 0)
+        cv2.moveWindow(self.params_win, 1020, 0)
         self.disp_controls()
         for pn in self.param_names:
-            cv2.createTrackbar(pn,self.window,int(self.params[pn]), 100, self.update_trackbar_params)
-
+            cv2.createTrackbar(pn,self.params_win,int(self.params[pn]), 100, self.update_trackbar_params)
+        self.update_trackbar_params(self)
+        
         # Set initial variables
         self.masks = {}
         self.mask_idxs = {}
@@ -83,7 +96,10 @@ class Experiment(object):
         self.init(trials=n_trials)
     def update_trackbar_params(self, _):
         for param in self.param_names:
-            self.params[param] = cv2.getTrackbarPos(param,self.window)
+            self.params[param] = cv2.getTrackbarPos(param,self.params_win)
+        self.params['wheel_translation'] -= 50
+        self.params['eye_translation'] -= 50
+        self.plotline.set_ydata([self.params['movement_std_threshold']+self.params['wheel_translation'] for i in range(self.params['movement_query_frames'])])
     def disp_controls(self):
         img = np.ones((150,170))*255
         lab_origin = 10
@@ -136,11 +152,11 @@ class Experiment(object):
             self.set_masks()
         
         # setup containers for acquired data
-        self.monitor_img_set = None
+        self.monitor_img_set = {n:None for n in self.mask_names}
+        self.monitor_vals = {m:[] for m in self.mask_names}
         self.TRIAL_ON = False
         self.TRIAL_PAUSE = False
         self.last_trial_off = pytime.time()
-        self.monitor_vals = []
         self.frame_count = 0
         
         # save metadata
@@ -163,6 +179,7 @@ class Experiment(object):
         for m in self.mask_names:
             print "Please select mask: %s."%m
             frame, timestamp = self.camera.read()
+            pl.figure()
             pl.imshow(frame, cmap=mpl_cm.Greys_r)
             pts = pl.ginput(0)
             pl.close()
@@ -178,60 +195,63 @@ class Experiment(object):
         try:
             self.camera.release()
             self.daq.release()
+            self.analog_daq.release()
             cv2.destroyAllWindows()
+            pl.close(self.fig)
         except:
             pass
     def query_for_trigger(self):
         if pytime.time()-self.last_trial_off < self.params['inter_trial_min']:
             return False
-        return self.monitor_vals[-1] < self.params['movement_std_threshold']
+        return self.monitor_vals[self.motion_mask][-1] < self.params['movement_std_threshold']
     def monitor_frame(self, frame):
-        if self.monitor_img_set != None:
-            self.monitor_img_set = np.dstack([self.monitor_img_set, frame])
-            
-            # only hold on to as many as you'll look at:
-            if np.shape(self.monitor_img_set)[-1]>self.params['movement_query_frames']:
-                self.monitor_img_set = self.monitor_img_set[...,-self.params['movement_query_frames']:]
-            
-            mask_idxs = self.mask_idxs[self.motion_mask]
-            std_pts = np.std(self.monitor_img_set[mask_idxs[0],mask_idxs[1],:], axis=1)
-            self.monitor_vals.append(np.mean(std_pts))
+        for mask in self.mask_names:
+            if self.monitor_img_set[mask] != None:
+                self.monitor_img_set[mask] = np.dstack([self.monitor_img_set[mask], frame])
+                
+                # only hold on to as many as you'll look at:
+                if np.shape(self.monitor_img_set[mask])[-1]>self.params['movement_query_frames']:
+                    self.monitor_img_set[mask] = self.monitor_img_set[mask][...,-self.params['movement_query_frames']:]
+                
+                mask_idxs = self.mask_idxs[mask]
+                if mask==self.motion_mask:
+                    std_pts = np.std(self.monitor_img_set[mask][mask_idxs[0],mask_idxs[1],:], axis=1)
+                    self.monitor_vals[mask].append(np.mean(std_pts))
+                else:
+                    mean_pts = np.mean(self.monitor_img_set[mask][mask_idxs[0],mask_idxs[1],:], axis=1)
+                    self.monitor_vals[mask].append(np.mean(mean_pts))
 
-            if len(self.monitor_vals)>self.params['movement_query_frames']:
-                self.monitor_vals = self.monitor_vals[-self.params['movement_query_frames']:]
-           
-            plot = self.imgplot()
-            cv2.imshow(self.plot, plot)
-        else:
-            self.monitor_img_set = frame
-    def imgplot(self):
-        mqf = self.params['movement_query_frames']
-        w = mqf
-        h = 50
-        plot = np.zeros((h,w))
-        vals = np.append(np.zeros(mqf-len(self.monitor_vals)),self.monitor_vals)
-        vals = np.round(vals).astype(np.uint8)
-        vals = (h-1)-vals
-        vals[np.where(vals>h-1)] = h-1
-        plot[vals, range(mqf)] = 255
-        plot[(h-1)-int(self.params['movement_std_threshold']), np.arange(np.shape(plot)[1])[::3]] = 100
-        newshape = tuple(np.array(np.shape(plot))*4)
-        plot = cv2.resize(plot, newshape)
-        return plot
+                if len(self.monitor_vals[mask])>self.params['movement_query_frames']:
+                    self.monitor_vals[mask] = self.monitor_vals[mask][-self.params['movement_query_frames']:]
+            else:
+                self.monitor_img_set[mask] = frame
+        if len(self.monitor_vals['EYE']) > 0:
+            tr = Trigger(msg=self.normalize(self.monitor_vals['EYE'][-1], minn=self.analog_daq.minn, maxx=self.analog_daq.maxx))
+            self.analog_daq.trigger(tr)
+        
+        toshow_w = np.array(self.monitor_vals['WHEEL']) * self.params['wheel_stretch']/10. + self.params['wheel_translation']
+        toshow_e = np.array(self.monitor_vals['EYE'])* self.params['eye_stretch']/10. + self.params['eye_translation']
+        if len(toshow_w) != self.params['movement_query_frames']:
+            toshow_w = np.append(toshow_w, np.array([None for _ in range(self.params['movement_query_frames']-len(toshow_w))]))
+            toshow_e = np.append(toshow_e, np.array([None for _ in range(self.params['movement_query_frames']-len(toshow_e))]))
+        self.plotdata['WHEEL'].set_ydata(toshow_w)
+        self.plotdata['EYE'].set_ydata(toshow_e)
+        self.fig.canvas.draw()
+    def normalize(self, val, minn, maxx):
+        return val/1000. * (maxx-minn) + minn
     def next_frame(self):
         frame, timestamp = self.camera.read()
         self.frame_count += 1
         if self.TRIAL_ON:
             self.writer.write(frame)
             self.time.append(timestamp)
-            #self.store_frame(frame, timestamp) #OLD, SLOW
-        elif not self.frame_count % self.resample: #if to elif
+        if not self.frame_count % self.resample:
             if not self.TRIAL_ON and not self.TRIAL_PAUSE:
                 self.monitor_frame(frame)
-                cv2.imshow(self.window, frame) #into if indentation
+            cv2.imshow(self.window, frame)
     def send_trigger(self):
         self.daq.trigger(self.trigger_cycle.next)
-        print "Sent trigger #%i"%(self.trial_count)
+        print "Sent trigger #%i"%(self.trial_count+1)
     def start_trial(self):
         self.TRIAL_ON = pytime.time()
         self.trial_count += 1
@@ -243,10 +263,10 @@ class Experiment(object):
                 i += 1
             self.filename = os.path.join(self.name,'trial%i_redo%i.npz'%(self.trial_count,i))
         
-        self.writer = cv2.VideoWriter(self.filename+'.avi',1,self.camera.frame_rate,frameSize=self.camera.resolution,isColor=False)
+        self.writer = cv2.VideoWriter(self.filename+'.avi',0,self.camera.frame_rate,frameSize=self.camera.resolution,isColor=False)
         self.time = []
-        self.monitor_img_set = None
-        self.monitor_vals = []
+        self.monitor_img_set = {n:None for n in self.mask_names}
+        self.monitor_vals = {m:[] for m in self.mask_names}
     def end_trial(self):
         self.TRIAL_ON = False
         self.last_trial_off = pytime.time()
@@ -255,13 +275,13 @@ class Experiment(object):
         self.filename = None
     def step(self):
         self.next_frame()
+        c = cv2.waitKey(1)
         
         if self.TRIAL_ON:
             if pytime.time()-self.TRIAL_ON >= self.trigger_cycle.current.duration:
                 self.end_trial()
 
         if not self.TRIAL_ON: 
-            c = cv2.waitKey(1) #from after next_frame to here
             
             if c == ord('p'):
                 self.TRIAL_PAUSE = True
@@ -310,7 +330,7 @@ class TriggerCycle(object):
         return md
 
 if __name__=='__main__':
-    cam =  Camera(idx=0, resolution=(320,240), frame_rate=187, color_mode=Camera.BW)
+    cam =  Camera(idx=0, resolution=(320,240), frame_rate=100, color_mode=Camera.BW)
 
     CS = Trigger(msg=[0,0,1,1], duration=5.0, name='CS')
     US = Trigger(msg=[0,0,0,1], duration=5.0, name='US')
